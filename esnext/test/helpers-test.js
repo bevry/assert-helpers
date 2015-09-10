793ba24141f81ec3a5326ba8186718002f9fa983
Benjamin Lupton <b@lupton.cc>
2015-09-11 05:13:54 +0800

    v2.0.0. Improvements.

--- esnext/test/helpers-test.js
+++ esnext/test/helpers-test.js
@@ -1,1 +1,10 @@
-console.log('no tests yet')
+// Import
+const joe = require('joe')
+const helpers = require('../../')
+
+// Tests
+joe.suite('assert-helpers', function (suite, test) {
+	test('no tests yet', function () {
+		console.log('nope, none')
+	})
+})
